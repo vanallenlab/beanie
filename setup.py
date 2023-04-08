c5b7878cbefd4479529923c66c26b9196fe500bc
Shreya Johri <shreyajohri20@gmail.com>
2023-04-08 19:09:42 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/vanallenlab/beanie",
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=reqs,
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v2.0",
